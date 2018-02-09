040bb73b28f36a2da19629fa595c73793281cbdd
hyuk <hyuk@DESKTOP-VMLLI5D>
2018-02-09 16:03:14 +0900

    20180209 github update test. SubClass.java println->print

--- Day20180208_spring/src/kr/co/daegu/SubClass.java
+++ Day20180208_spring/src/kr/co/daegu/SubClass.java
@@ -3,7 +3,7 @@ package kr.co.daegu;
 public class SubClass implements Hap {
 	
 		public void clas(NumClass nc) {
-			System.out.println(nc.getNum1()+"-"+nc.getNum2()+"="+(nc.getNum1()-nc.getNum2()));
+			System.out.print(nc.getNum1()+"-"+nc.getNum2()+"="+(nc.getNum1()-nc.getNum2()));
 	
 		
 	}
